657fde71c772a4e1ca55916f778c60bf5adaec4f
David-Crumps <david.crumplin@gmail.com>
2026-03-05 10:47:10 +0200

    Added -> Crop card found on /crop/:id  which contains all necessary info for that specific crop

--- StardewValleyProfitSimulator - Frontend/src/components/Crop.jsx
+++ StardewValleyProfitSimulator - Frontend/src/components/Crop.jsx
@@ -1,16 +1,60 @@
 import Card from 'react-bootstrap/Card';
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
 
 const Crop = () => {
+  const { id } = useParams();
+  const [crop, setCrop] = useState(null);
+
+  useEffect(() => {
+    const fetchCrop = async () => {
+      try {
+        const response = await axios.get(
+          `http://localhost:8080/api/crops/${id}`,
+        );
+        setCrop(response.data);
+      } catch (error) {
+        console.error('Error fetching crop: ', error);
+      }
+    };
+    fetchCrop();
+  }, [id]);
+
+  if (!crop) {
+    return (
+      <h2 className="text-center" style={{ padding: '10rem' }}>
+        Loading...
+      </h2>
+    );
+  }
+
   return (
     <div className="centre-wrapper">
       <Card className="single-crop-card">
-        <Card.Body>
-          <Card.Title as={'h2'} className="crop-card-title">
-            Blue Jazz
-          </Card.Title>
-          <Card.Subtitle className="crop-card-subtitle">Spring</Card.Subtitle>
-          <Card.Text>Base Sell-Price: 50g</Card.Text>
-          <Card.Text>Cost: 30g</Card.Text>
+        <Card.Body className="d-flex align-items-center">
+          <Card.Img
+            alt="Picture of Crop"
+            variant="bottom"
+            src={`http://localhost:8080${crop.ImageUrl}`}
+            className="single-crop-img-card"
+          />
+          <div className="ms-3 single-crop-info-card">
+            <Card.Title as={'h2'} className="crop-card-title">
+              {crop.name}
+            </Card.Title>
+            <Card.Subtitle className="crop-card-subtitle">
+              {crop.seasons.map(({ name }) => name).join(', ')}
+            </Card.Subtitle>
+            <Card.Text>Base Sell-Price: {crop.cropSellPrice}g</Card.Text>
+            <Card.Text>Cost: {crop.seedCost}g</Card.Text>
+            <Card.Text>Seed sell-price: {crop.seedSellPrice}g</Card.Text>
+            <Card.Text>Inital Grow Time: {crop.growTime} days</Card.Text>
+            <Card.Text>
+              Regrowth Time:{' '}
+              {crop.regrowthTime === 0 ? 'N/A' : `${crop.regrowthTime} days`}
+            </Card.Text>
+          </div>
         </Card.Body>
       </Card>
     </div>
