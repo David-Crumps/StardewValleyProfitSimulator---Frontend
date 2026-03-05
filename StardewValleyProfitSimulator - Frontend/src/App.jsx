5bf5cf28d0d2ea4c7739f77ed90b29efc019f0e5
David-Crumps <david.crumplin@gmail.com>
2026-03-05 10:46:13 +0200

    Added Route /crop/:id to the Route Table

--- StardewValleyProfitSimulator - Frontend/src/App.jsx
+++ StardewValleyProfitSimulator - Frontend/src/App.jsx
@@ -11,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/crop" element={<Crop />} />
+        <Route path="/crop/:id" element={<Crop />} />
       </Routes>
     </>
   );
